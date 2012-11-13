6582a3b5cfc9207d97d5a50a3fd49fdc145302a8
James Hart <hjhart@gmail.com>
2012-11-12 20:22:52 -0800

    Basic rails framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Standup::Application.config.secret_token = '3fa5c11e57093451f63d04d365eed36cf4010d5038a5c13c11c6db9c327a4cb41dd09cd0f8f860f25aa5c7c7fd815dc7cc9a959e427cfec3bc238a92b677d630'
